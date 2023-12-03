637db92f0c290b1ae8f2c2de16fa2e60dbeb4499
BALA MURALI KRISHNA PENUMARTHI <Balu5a3@users.noreply.github.com>
2023-12-02 20:50:37 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class MyGeneric<T>
+    {
+        public T[] list;
+
+        public MyGeneric(int size)
+        {
+            list = new T[size];
+        }
+
+        public T getItem(int index)
+        {
+            return list[index];
+        }
+
+        public void setItem(int index, T value)
+        {
+            list[index] = value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGeneric<int> IntBalu = new MyGeneric<int>(5);
+            IntBalu.setItem(4, 1);
+            int a = IntBalu.getItem(4);
+            MyGeneric<string> StringBalu = new MyGeneric<string>(5);
+        }
+    }
+}
